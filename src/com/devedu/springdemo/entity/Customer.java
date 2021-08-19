5bf4749904be3ab27fe991ed8ddd07bf65141776
Eduardo Guimaraes <eduguimara@gmail.com>
2021-08-18 22:34:39 -0300

    Create Customer entity

---
+++ src/com/devedu/springdemo/entity/Customer.java
@@ -0,0 +1,66 @@
+package com.devedu.springdemo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+	@Id
+	@Column(name = "id")
+	private long id;
+	
+	@Column(name = "first_name")
+	private String fistName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "email")
+	private String email;
+	
+	public Customer() {
+	}
+
+	public Customer(String fistName, String lastName, String email) {
+		this.fistName = fistName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFistName() {
+		return fistName;
+	}
+
+	public void setFistName(String fistName) {
+		this.fistName = fistName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
