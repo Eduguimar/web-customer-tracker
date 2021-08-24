eaff0368a9ce1a0e6a06d6a299446752bd262d10
Eduardo Guimaraes <eduguimara@gmail.com>
2021-08-23 22:07:05 -0300

    Create CustomerDAO and CustomerDAOImpl

---
+++ src/com/devedu/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.devedu.springdemo.dao;
+
+import java.util.List;
+
+import com.devedu.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+	
+	public List<Customer> getCustomers();
+
+}
--- src/com/devedu/springdemo/entity/Customer.java
+++ src/com/devedu/springdemo/entity/Customer.java
@@ -66,4 +66,6 @@ public class Customer {
 		this.email = email;
 	}
 	
+	
+	
 }
